977944767b161cdc0bf805874de45ece42ddec19
hemanthnb <hemanthnb3@gmail.com>
2023-11-01 15:24:10 +0530

    implemented props and proptypes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -23,7 +23,6 @@ export default function NavBar(props) {
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
