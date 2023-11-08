6fc93bdbe3344f7763a1bdc0cdda0d24eb8e5fc3
hemanthnb <hemanthnb3@gmail.com>
2023-11-08 22:31:36 +0530

    added feature for  count of words, uppercase and lowercase

--- src/Components/TempFile.js
+++ src/Components/TempFile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TempFile(props) {
 
-    // We are converting it to string because we cannot directl sendy 
+    // We are converting it to string because we cannot directly send 
     const str=JSON.stringify(props.jsondata)
   return (
       <div>JsonData= {str}</div>
